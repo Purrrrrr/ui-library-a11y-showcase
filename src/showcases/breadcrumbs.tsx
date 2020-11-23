943d25733e072ffca4eb49593fb9d360f00f11d6
Purr Consuegra <purr.consuegra@solita.fi>
2020-11-23 19:13:55 +0200

    Delete unused files

--- src/showcases/breadcrumbs.tsx
+++
@@ -1,6 +0,0 @@
-import {blueprintBreadcrumbs} from './blueprint';
-import {bootstrapBreadcrumbs} from './bootstrap';
-import {materialBreadcrumbs} from './materialDesign';
-
-const breadcrumbsShowcases = [materialBreadcrumbs, bootstrapBreadcrumbs, blueprintBreadcrumbs];
-export default breadcrumbsShowcases;
--- src/showcases/buttons.tsx
+++
@@ -1,6 +0,0 @@
-import {blueprintButton} from './blueprint';
-import {bootstrapButton} from './bootstrap';
-import {materialButton} from './materialDesign';
-
-const buttonShowcases = [materialButton, blueprintButton, bootstrapButton];
-export default buttonShowcases;
